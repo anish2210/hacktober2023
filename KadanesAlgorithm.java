3caabf7f3e57453571df64634a52cfcb909ac5a5
Sahil Singh <imgohan444@gmail.com>
2023-10-02 15:19:03 +0530

    create kadane's algorithm

---
+++ KadanesAlgorithm.java
@@ -0,0 +1,22 @@
+// Find the maximum subarray sum (Kadane's algorithm)
+
+public class KadanesAlgorithm {
+
+    public static int maxSubArraySum(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = maxSubArraySum(nums);
+        System.out.println("Maximum subarray sum is: " + maxSum);
+    }
+}
